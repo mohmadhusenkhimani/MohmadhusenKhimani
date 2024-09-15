1ece36278cbbbdf8c8b38d034ac60f140f0c8526
Mohmadhusen Khimani <143099310+mkhimani452@users.noreply.github.com>
2024-09-15 16:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Select elements from the DOM
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+let themeToggler = document.querySelector('#theme-toggler');
+
+// Toggle menu visibility
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+};
+
+// Toggle theme between light and dark
+themeToggler.onclick = () => {
+    // Check if the themeToggler currently has the 'fa-sun' class
+    if (themeToggler.classList.contains('fa-sun')) {
+        // If it does, remove the 'fa-sun' class and add 'fa-moon'
+        themeToggler.classList.remove('fa-sun');
+        themeToggler.classList.add('fa-moon');
+        document.body.classList.remove('active');
+    } else {
+        // Otherwise, add 'fa-sun' class and remove 'fa-moon'
+        themeToggler.classList.remove('fa-moon');
+        themeToggler.classList.add('fa-sun');
+        document.body.classList.add('active');
+    }
+};
 
